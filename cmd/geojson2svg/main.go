7f247679efd0532c51df889781285e16d59949b8
thisisaaronland <devnull@localhost>
2019-12-14 13:22:37 -0800

    update vendor deps

--- cmd/geojson2svg/main.go
+++ cmd/geojson2svg/main.go
@@ -4,17 +4,17 @@ import (
 	"flag"
 	"fmt"
 	"github.com/whosonfirst/go-geojson-svg"
+	"io/ioutil"
 	"log"
 	"os"
-	"io/ioutil"
 )
 
 func main() {
 
 	height := flag.Float64("height", 640., "")
-	width := flag.Float64("width", 1024., "")	
+	width := flag.Float64("width", 1024., "")
 	mercator := flag.Bool("mercator", false, "")
-	
+
 	flag.Parse()
 
 	s := svg.New()
@@ -40,13 +40,13 @@ func main() {
 
 		if err != nil {
 			log.Fatal(err)
-		}		
+		}
 	}
 
 	out := s.Draw(*width, *height,
 		svg.WithAttribute("xmlns", "http://www.w3.org/2000/svg"),
 		svg.WithAttribute("viewBox", fmt.Sprintf("0 0 %d %d", int(*width), int(*height))),
-	);
-	
+	)
+
 	fmt.Println(out)
 }
